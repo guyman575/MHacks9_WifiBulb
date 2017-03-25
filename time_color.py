df02c9f8f5de09f2155863eb23937841bb25c3bb
Guy Cassetta <rgcassetta@gmail.com>
2017-03-25 14:36:56 -0400

    time color stub added

---
+++ time_color.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+#TODO Return a hex value as a string for the color based on the time of day
+#NOTE: Format of collors are RRGGBBWW, if a value other than 00 is assigned to WW
+#       Then the bulb will only show white. I think that ff is full brightness and 00
+#       is off. You should use values like 000000ff. See speed_color.py for example
+#       on having a range of values. or do it your own way.
+def check_time():
+
+
+    return
