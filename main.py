a67809f01bd55bfdd273e7e604d3c56b35f7c44a
Guy Cassetta <rgcassetta@gmail.com>
2017-03-25 04:09:18 -0400

    its 4 am

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-test
+import os
+from time import sleep
+
+RED = '56ff000000f0aa'
+GREEN = '5600ff0000f0aa'
+BLUE = '560000ff00f0aa'
+WHITE = '56fffffffff0aa'
+OFF = '5600000000f0aa'
+
+def change_color(color):
+    os.system('sudo gatttool -b D2:39:79:10:18:87 --char-write-req --handle=0x0043 --value=' + color)
+
+def main():
+    print("HI")
+
+    while True:
+        sleep(.1)
+        change_color(RED)
+        sleep(.1)
+        change_color(GREEN)
+        sleep(.1)
+        change_color(BLUE)
+
+if __name__ == "__main__":
+    main()
---
+++ off.sh
@@ -0,0 +1,1 @@
+sudo gatttool -b D2:39:79:10:18:87 --char-write-req --handle=0x0043 --value=5600000000f0aa
